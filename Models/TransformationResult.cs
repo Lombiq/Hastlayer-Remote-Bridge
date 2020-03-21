f6f1f09fb7dff1600ba9cb4441c35a6793855855
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2020-03-21 14:56:52 +0100

    The Hastlayer version is now also transmitted

--- Models/TransformationResult.cs
+++ Models/TransformationResult.cs
@@ -4,6 +4,7 @@ namespace Hast.Remote.Bridge.Models
 {
     public class TransformationResult
     {
+        public string RemoteHastlayerVersion { get; set; }
         public HardwareDescription HardwareDescription { get; set; }
         public IEnumerable<string> Errors { get; set; }
     }
