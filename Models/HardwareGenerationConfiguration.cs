b0a9f6588d865d65fae91f355b1e1cdc30aec9c5
Dávid El-Saig <david.el-saig@lombiq.com>
2020-12-06 06:13:40 +0100

    Remove setter from collection properties.

--- Models/HardwareGenerationConfiguration.cs
+++ Models/HardwareGenerationConfiguration.cs
@@ -1,12 +1,25 @@
-﻿using System.Collections.Generic;
+#nullable enable
+using System.Collections.Generic;
 
 namespace Hast.Remote.Bridge.Models
 {
     public class HardwareGenerationConfiguration
     {
-        public IDictionary<string, object> CustomConfiguration { get; set; }
-        public IList<string> HardwareEntryPointMemberFullNames { get; set; }
-        public IList<string> HardwareEntryPointMemberNamePrefixes { get; set; }
         public string DeviceName { get; set; }
+        public IDictionary<string, object> CustomConfiguration { get; }
+        public IList<string> HardwareEntryPointMemberFullNames { get; }
+        public IList<string> HardwareEntryPointMemberNamePrefixes { get; }
+
+        public HardwareGenerationConfiguration(
+            string deviceName,
+            IDictionary<string, object>? customConfiguration = null,
+            IList<string>? hardwareEntryPointMemberFullNames = null,
+            IList<string>? hardwareEntryPointMemberNamePrefixes = null)
+        {
+            DeviceName = deviceName;
+            CustomConfiguration = customConfiguration ?? new Dictionary<string, object>();
+            HardwareEntryPointMemberFullNames = hardwareEntryPointMemberFullNames ?? new List<string>();
+            HardwareEntryPointMemberNamePrefixes = hardwareEntryPointMemberNamePrefixes ?? new List<string>();
+        }
     }
 }
