2f3de75f1d2c2e5ee0933d42d732f767980180cf
Dávid El-Saig <david.el-saig@lombiq.com>
2021-12-22 03:37:28 +0100

    Fix AssemblyContainer's POCOness.

--- Models/AssemblyContainer.cs
+++ Models/AssemblyContainer.cs
@@ -1,4 +1,8 @@
+using Newtonsoft.Json;
 using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Hast.Remote.Bridge.Models
 {
@@ -6,15 +10,26 @@ namespace Hast.Remote.Bridge.Models
     {
         public string Name { get; set; }
 
-        private byte[] _fileContent;
+        // Using a property with the [JsonProperty] attribute makes it serializable even when it's otherwise inaccessible.
+        [JsonProperty]
+        private byte[] FileContent { get; set; }
 
         public AssemblyContainer(string name, byte[] fileContent)
         {
             Name = name ?? throw new ArgumentNullException(nameof(name));
-            _fileContent = fileContent ?? throw new ArgumentNullException(nameof(fileContent));
+            FileContent = fileContent ?? throw new ArgumentNullException(nameof(fileContent));
         }
 
-        public byte[] GetFileContent() => _fileContent;
-        public void SetFileContent(byte[] value) => _fileContent = value;
+        public async Task SaveContentToFileStreamAsync(FileStream fileStream, CancellationToken cancellationToken = default)
+        {
+            // See: https://github.com/microsoft/referencesource/blob/master/mscorlib/system/io/stream.cs#L50-L53
+            const int defaultBufferSize = 81920;
+
+            using var memoryStream = new MemoryStream(FileContent);
+            await memoryStream.CopyToAsync(
+                fileStream,
+                Math.Min(FileContent.Length, defaultBufferSize),
+                cancellationToken);
+        }
     }
 }
